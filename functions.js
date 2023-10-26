e707e239cec46351c10a3995363ea8b07f643a5b
Kilian Boute <kilian.boute@gmail.com>
2023-10-26 13:53:15 +0200

    fix js issues

---
+++ functions.js
@@ -0,0 +1,13 @@
+
+   
+
+
+
+    function mobileDropdown() {
+        var x = document.getElementById("links");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
